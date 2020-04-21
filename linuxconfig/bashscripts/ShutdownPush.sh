4bb15b1d186267ae656c9db3ddbf5b1356f7c53d
Ezra Haleva <HalevaEzra@gmail.com>
2020-04-21 09:10:00 -0400

    Tue Apr 21 09:10:00 EDT 2020

--- linuxconfig/bashscripts/ShutdownPush.sh
+++ linuxconfig/bashscripts/ShutdownPush.sh
@@ -1,10 +1,10 @@
 cd ~/GithubDrive
 git add *
 git commit -m "$(date)"
-git push origin master >> "shutdownPushLog.txt"
+git push origin master > "shutdownPushLog.txt"
 
 cd ~/Journal
 git add *
 git commit -m "$(date)"
-git push origin master >> "shutdownPushLog.txt"
+git push origin master > "shutdownPushLog.txt"
 
